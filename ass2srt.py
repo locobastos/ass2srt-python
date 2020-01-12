c0753cc6230cec2996256506bd58e8adb8e6b06e
locobastos <bastien.thierry.martin@gmail.com>
2020-01-12 13:47:40 +0100

    Refactor to test all text modification.

--- ass2srt.py
+++ ass2srt.py
@@ -77,72 +77,156 @@ def convert_ass_to_srt(ass_filename):
             exit(10)
 
 
+def replace_ass_n_by_system_n(str_temp):
+    return str_temp.replace(r'\N', '\n')
+
+
+def replace_italic_tags(str_temp):
+    return str_temp.replace(r'{\i1}', '<i>').replace(r'{\i0}', '</i>')
+
+
+def replace_bold_tags(str_temp):
+    return str_temp.replace(r'{\b1}', '<b>').replace(r'{\b0}', '</b>')
+
+
+def remove_b100_to_b900_explicit_bold_weight(str_temp):
+    return re.sub(r'{\\b[1-9]00}', '', str_temp)
+
+
+def replace_underline_tags(str_temp):
+    return str_temp.replace(r'{\u1}', '<u>').replace(r'{\u0}', '</u>')
+
+
+def remove_strikeout_tags(str_temp):
+    return re.sub(r'\{\\s[0-1]\}', '', str_temp)
+
+
+def remove_border_tags_and_extended(str_temp):
+    return re.sub(r'\{\\[x-y]?bord[0-9]*\.?[0-9]*\}', '', str_temp)
+
+
+def remove_shadow_distance_and_extended(str_temp):
+    return re.sub(r'\{\\[x-y]?shad[0-9]*\.?[0-9]*\}', '', str_temp)
+
+
+def remove_blur_edge_gaussian_kernel(str_temp):
+    return re.sub(r'\{\\blur[0-9]*\.?[0-9]*\}', '', str_temp)
+
+
+def remove_font_name(str_temp):
+    regex_fonts_name = "|".join(fonts_name_list)
+    return re.sub(r'{\\fn(' + regex_fonts_name + ')}', '', str_temp)
+
+
+def remove_font_size(str_temp):
+    return re.sub(r'\{\\fs[1-9]*\}', '', str_temp)
+
+
+def remove_font_scale(str_temp):
+    return re.sub(r'\{\\fsc[x-y]?[0-9]*\}', '', str_temp)
+
+
+def remove_letter_spacing(str_temp):
+    return re.sub(r'\{\\fsp\-?[0-9]*\.?[0-9]*\}', '', str_temp)
+
+
+def remove_text_rotation(str_temp):
+    return re.sub(r'\{\\fr[x-z]?[0-9]*\}', '', str_temp)
+
+
+def remove_text_shearing(str_temp):
+    return re.sub(r'\{\\fa[x-y]+[0-9]*\.?[0-9]*\}', '', str_temp)
+
+
+def remove_font_encoding(str_temp):
+    return re.sub(r'\{\\fe[0-9]*\}', '', str_temp)
+
+
+def remove_text_color(str_temp):
+    return re.sub(r'\{\\[1-4]?c\&H[0-9a-fA-F]*&\}', '', str_temp)
+
+
+def remove_transparency_text_alpha(str_temp):
+    return re.sub(r'\{\\[1-4]?a(?:lpha)?\&H[0-9a-fA-F]*\}', '', str_temp)
+
+
+def remove_line_alignment(str_temp):
+    return re.sub(r'\{\\a[n]?[0-9]+\}', '', str_temp)
+
+
+def remove_karaoke_effect(str_temp):
+    return re.sub(r'\{\\[k,K][f,o]?[0-9]*\}', '', str_temp)
+
+
+def remove_wrap_style(str_temp):
+    return re.sub(r'\{\\q[0-3]*\}', '', str_temp)
+
+
+def remove_reset_style(str_temp):
+    regex_style_names = "|".join(style_list)
+    return re.sub(r'{\\r(' + regex_style_names + ')?}', '', str_temp)
+
+
+def remove_text_position(str_temp):
+    return re.sub(r'\{\\pos\([0-9]*\,[0-9]*\)\}', '', str_temp)
+
+
+def remove_movement(str_temp):
+    return re.sub(r'\{\\move\([0-9]*\,[0-9]*\,[0-9]*\,[0-9]*\,?[0-9]*\,?[0-9]*\)\}', '', str_temp)
+
+
+def remove_rotation(str_temp):
+    return re.sub(r'\{\\org\([0-9]*\,[0-9]*\)\}', '', str_temp)
+
+
+def remove_fade(str_temp):
+    return re.sub(r'\{\\fad\([0-9]*\,[0-9]*\,?[0-9]*\,?[0-9]*\,?[0-9]*\,?[0-9]*\,?[0-9]*\)\}', '', str_temp)
+
+
+def remove_animated_transform(str_temp):
+    return re.sub(r'\\t\(\\?[0-9a-zA-Z&]*\.?[0-9]*\,?\\?[0-9a-zA-Z&]*\.?[0-9]*\,?\\?[0-9a-zA-Z&]*\.?[0-9]*\,?\\?[0-9a-zA-Z&]*\.?[0-9]*\)\}', '', str_temp)
+
+
+def remove_clip_rectangle(str_temp):
+    return re.sub(r'\{\\[i]?clip\([0-9]*\,[0-9]*\,[0-9]*\,[0-9]*\\)\}', '', str_temp)
+
+
 def remove_ass_tags(str_input):
     # Remove current \n
     str_temp = str_input.replace('\n', '')
-    # Replace ASS \N by \n
-    str_temp = str_temp.replace(r'\N', '\n')
+    str_temp = replace_ass_n_by_system_n(str_temp)
 
     # Source of all ASS Tags: http://docs.aegisub.org/3.2/ASS_Tags/
-    # Replace italic tags
-    str_temp = str_temp.replace(r'{\i1}', '<i>').replace(r'{\i0}', '</i>')
-    # Replace bold tags
-    str_temp = str_temp.replace(r'{\b1}', '<b>').replace(r'{\b0}', '</b>')
-    # Remove {\b100} to {\b900} explicit bold weight
-    re.sub(r'{\\b[1-9]00}', '', str_temp)
-    # Replace underline tags
-    str_temp = str_temp.replace(r'{\u1}', '<u>').replace(r'{\u0}', '</u>')
-    # Remove Strikeout tags
-    re.sub(r'\{\\s[0-1]\}', '', str_temp)
-    # Remove border tags + extended
-    re.sub(r'\{\\[x-y]?bord[0-9]*\.?[0-9]*\}', '', str_temp)
-    # Remove shadow distance + extended
-    re.sub(r'\{\\[x-y]?shad[0-9]*\.?[0-9]*\}', '', str_temp)
-    # Remove blur edge (Gaussian kernel)
-    re.sub(r'\{\\blur[0-9]*\.?[0-9]*\}', '', str_temp)
-    # Remove font name
-    regex_fonts_name = "|".join(fonts_name_list)
-    re.sub(r'{\\fn(' + regex_fonts_name + ')}', '', str_temp)
-    # Remove font size
-    re.sub(r'\{\\fs[1-9]*\}', '', str_temp)
-    # Remove font scale
-    re.sub(r'\{\\fsc[x-y]?[0-9]*\}', '', str_temp)
-    # Remove letter spacing
-    re.sub(r'\{\\fsp\-?[0-9]*\.?[0-9]*\}', '', str_temp)
-    # Remove text rotation
-    re.sub(r'\{\\fr[x-z]?[0-9]*\}', '', str_temp)
-    # Remove text shearing
-    re.sub(r'\{\\fa[x-y]+[0-9]*\.?[0-9]*\}', '', str_temp)
-    # Remove font encoding
-    re.sub(r'\{\\fe[0-9]*\}', '', str_temp)
-    # Remove text color
-    re.sub(r'\{\\[1-4]?c\&H[0-9a-fA-F]*&\}', '', str_temp)
-    # Remove transparency text (alpha)
-    re.sub(r'\{\\[1-4]?a(?:lpha)?\&H[0-9a-fA-F]*\}', '', str_temp)
-    # Remove line alignment
-    re.sub(r'\{\\a[n]?[0-9]+\}', '', str_temp)
-    # Remove karaoke effect
-    re.sub(r'\{\\[k,K][f,o]?[0-9]*\}', '', str_temp)
-    # Remove wrap style
-    re.sub(r'\{\\q[0-3]*\}', '', str_temp)
-    # Remove reset style
-    regex_style_names = "|".join(style_list)
-    re.sub(r'{\\r(' + regex_style_names + ')?}', '', str_temp)
-    # Remove text position
-    re.sub(r'\{\\pos\([0-9]*\,[0-9]*\)\}', '', str_temp)
-    # Remove movement
-    re.sub(r'\{\\move\([0-9]*\,[0-9]*\,[0-9]*\,[0-9]*\,?[0-9]*\,?[0-9]*\)\}', '', str_temp)
-    # Remove rotation
-    re.sub(r'\{\\org\([0-9]*\,[0-9]*\)\}', '', str_temp)
-    # Remove fade
-    re.sub(r'\{\\fad\([0-9]*\,[0-9]*\,?[0-9]*\,?[0-9]*\,?[0-9]*\,?[0-9]*\,?[0-9]*\)\}', '', str_temp)
-    # Remove animated transform
-    re.sub(r'\\t\(\\?[0-9a-zA-Z&]*\.?[0-9]*\,?\\?[0-9a-zA-Z&]*\.?[0-9]*\,?\\?[0-9a-zA-Z&]*\.?[0-9]*\,?\\?[0-9a-zA-Z&]*\.?[0-9]*\)\}', '', str_temp)
-    # Remove clip (rectangle)
-    re.sub(r'\{\\[i]?clip\([0-9]*\,[0-9]*\,[0-9]*\,[0-9]*\\)\}', '', str_temp)
+    str_temp = replace_italic_tags(str_temp)
+    str_temp = replace_bold_tags(str_temp)
+    str_temp = remove_b100_to_b900_explicit_bold_weight(str_temp)
+    str_temp = replace_underline_tags(str_temp)
+    str_temp = remove_strikeout_tags(str_temp)
+    str_temp = remove_border_tags_and_extended(str_temp)
+    str_temp = remove_shadow_distance_and_extended(str_temp)
+    str_temp = remove_blur_edge_gaussian_kernel(str_temp)
+    str_temp = remove_font_name(str_temp)
+    str_temp = remove_font_size(str_temp)
+    str_temp = remove_font_scale(str_temp)
+    str_temp = remove_letter_spacing(str_temp)
+    str_temp = remove_text_rotation(str_temp)
+    str_temp = remove_text_shearing(str_temp)
+    str_temp = remove_font_encoding(str_temp)
+    str_temp = remove_text_color(str_temp)
+    str_temp = remove_transparency_text_alpha(str_temp)
+    str_temp = remove_line_alignment(str_temp)
+    str_temp = remove_karaoke_effect(str_temp)
+    str_temp = remove_wrap_style(str_temp)
+    str_temp = remove_reset_style(str_temp)
+    str_temp = remove_text_position(str_temp)
+    str_temp = remove_movement(str_temp)
+    str_temp = remove_rotation(str_temp)
+    str_temp = remove_fade(str_temp)
+    str_temp = remove_animated_transform(str_temp)
+    str_temp = remove_clip_rectangle(str_temp)
+
     # Add \n at the end of the line
     str_temp += '\n\n'
-
     return str_temp
 
 # _____ MAiN ___________________________________________________________________________________________________________
